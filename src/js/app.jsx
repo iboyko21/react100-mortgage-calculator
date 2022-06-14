486e8ec1c00b39bfdaa791d8c451c5f2f017609d
Igor Boyko <iboyko@gmail.com>
2022-06-14 16:18:33 -0700

    tied form inputs to update state

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,48 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import './style.css';
 
-export default class App extends React.Component {
+export default class App extends Component {
   // your Javascript goes here
+  constructor(props){
+    super(props);
+      this.state = {
+        balance: 0,
+        rate: 0,
+        term: 15
+      };
+      this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleOnChange(event) {
+      this.setState({
+          [event.target.id]: event.target.value
+      });
+      console.log(this.state);
+  }
+  
   render() {
     return (
       <div className="container">
-        <h1>Mortgage Calculator</h1>
+        <h1 className="text-center">Mortgage Calculator</h1>
+        <hr className="w-25" />
       
           <form className="form-horizontal">
             <div className="form-group">
               <label for="balance" className="col-sm-2 control-label">Loan Balance</label>
               <div className="col-sm-6">
-                <input type="number" className="form-control" id="balance" name="balance" placeholder="Your Loan Balance" />
+                <input type="number" className="form-control" id="balance" name="balance" value={this.state.balance} onChange={this.handleOnChange} placeholder="Your Loan Balance" />
               </div>
             </div>
 
             <div className="form-group">
-              <label for="rate" className="col-sm-2 control-label">APR</label>
+              <label for="rate" className="col-sm-2 control-label">Interest Rate (%)</label>
               <div className="col-sm-6">
-                <input type="number" className="form-control" id="rate" placeholder="Annual Percentage Rate" />
+                <input type="number" step="0.01" className="form-control" id="rate" name="rate" value={this.state.rate} onChange={this.handleOnChange} placeholder="Annual Percentage Rate" />
               </div>
             </div>
 
             <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-2">
-              <label for="term" className="col-sm-2 control-label">Term</label>
-                <select name="term" id="term" className="form-control">
-                  <option value="15">15 years</option>
-                  <option value="30">30 years</option>
+              <label for="term" className="col-sm-2 control-label">Loan Term (years)</label>
+              <div className="col-sm-6">
+                <select className="form-control" id="term" name="term" value={this.state.term} onChange={this.handleOnChange}>
+                  <option value="15">15</option>
+                  <option value="30">30</option>
                 </select>
               </div>
             </div>
 
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-2">
-                <button type="submit" className="btn btn-primary">Calculate</button>
+                <button name="submit" className="btn btn-primary">Calculate</button>
               </div>
             </div>
           </form>
 
-          <div className="output">
-
+          <div className="output" id="output">
+            Output
           </div>
 
       </div>
