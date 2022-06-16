6b5aeb98a4689d2a98d7098fdefe70a140aadd63
Igor Boyko <iboyko@gmail.com>
2022-06-16 16:31:42 -0700

    final working version

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,13 +5,14 @@ export default class App extends Component {
   constructor(props){
     super(props);
       this.state = {
-        balance: 0,
-        rate: 0,
+        balance: "",
+        rate: "",
         term: 15,
-        mPay: 0
+        mPay: ""
       };
       this.handleOnChange = this.handleOnChange.bind(this);
       this.handleOnClick = this.handleOnClick.bind(this);
+      this.handleReset = this.handleReset.bind(this);
     }
 
     handleOnChange(event) {
@@ -28,9 +29,18 @@ export default class App extends Component {
 
       let result = (balance * (rate * Math.pow((1 + rate), term)) / 
                     (Math.pow((1 + rate), term) - 1)).toFixed(2);
-
+                    
       this.setState({
         mPay: result
+      })
+    }
+
+    handleReset() {
+      this.setState({
+        balance: "",
+        rate: "",
+        term: 15,
+        mPay: ""
       })
     }
   
@@ -38,25 +48,25 @@ export default class App extends Component {
     return (
       <div className="container">
           <h3>Mortgage Calculator</h3>
-        <hr />
-          <form className="form-horizontal">
+        
+          <form className="jumbotron form-horizontal">
             <div className="form-group">
-              <label for="balance" className="col-sm-2 control-label">Loan Balance</label>
-              <div className="col-sm-6">
+              <label htmlFor="balance" className="col-sm-4 control-label">Loan Balance</label>
+              <div className="col-sm-4">
                 <input type="number" className="form-control" id="balance" name="balance" value={this.state.balance} onChange={this.handleOnChange} />
               </div>
             </div>
 
             <div className="form-group">
-              <label for="rate" className="col-sm-2 control-label">Interest Rate (%)</label>
-              <div className="col-sm-6">
+              <label htmlFor="rate" className="col-sm-4 control-label">Interest Rate (%)</label>
+              <div className="col-sm-4">
                 <input type="number" step="0.01" className="form-control" id="rate" name="rate" value={this.state.rate} onChange={this.handleOnChange} />
               </div>
             </div>
 
             <div className="form-group">
-              <label for="term" className="col-sm-2 control-label">Loan Term (years)</label>
-              <div className="col-sm-6">
+              <label htmlFor="term" className="col-sm-4 control-label">Loan Term (years)</label>
+              <div className="col-sm-4">
                 <select className="form-control" id="term" name="term" value={this.state.term} onChange={this.handleOnChange}>
                   <option value="15">15</option>
                   <option value="30">30</option>
@@ -65,15 +75,16 @@ export default class App extends Component {
             </div>
 
             <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-2">
-                <button name="submit" onClick={this.handleOnClick} className="btn btn-primary">Calculate</button>
+              <div className="col-sm-offset-4 col-sm-4">
+                <button type="button" className="btn btn-primary" name="submit" onClick={this.handleOnClick}>Calculate</button> 
+                <button type="button" className="btn btn-danger" name="reset" onClick={this.handleReset}>Reset</button>
               </div>
             </div>
           </form>
 
-          <div className="col-sm-offset-4" id="output">
-           ${this.state.mPay} is your payment
-          </div>
+            <div className="col-sm-offset-4 col-sm-4" id="output">
+              <b>${this.state.mPay}</b> is your payment
+            </div>
       </div>
     );
   }
